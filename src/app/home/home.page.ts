4bb1105996bec160f6c87f18a411f530b1eb7c64
aavila89 <90415322+aavila89@users.noreply.github.com>
2023-09-02 16:52:13 -0400

    feat: Implemented @capacitor/share plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Share } from '@capacitor/share';
+import { Camera, CameraResultType, ImageOptions } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  options: ImageOptions = {
+    quality: 90,
+    allowEditing: true,
+    resultType: CameraResultType.Uri
+  };
   constructor() {}
 
+  async shareInfo() {
+    const {value}  = await Share.canShare();
+    if (value) {
+      await Share.share({
+        title: 'Ionic plugins expert',
+        text: 'Welcome to my YouTube channel',
+        url: 'https://medium.com/@avilaatencioa',
+        dialogTitle: 'Share with buddies',
+      });
+    }
+  }
+
+  async shareImage() {
+    // Share local file using url parameter
+    const {value}  = await Share.canShare();
+    if (value) {
+      const photo = await Camera.getPhoto(this.options);
+      await Share.share({
+        url: photo.path,
+      });
+    }
+  }
 }
